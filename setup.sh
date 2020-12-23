881cffc81b6433499dc4dd95c10affce8cd84cd2
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-12-23 22:29:50 +0200

    feat(software): add software setup

---
+++ setup.sh
@@ -0,0 +1,40 @@
+xcode-select --install
+
+git config --global user.name $NAME
+git config --global user.email $EMAIL
+git config --global credential.helper osxkeychain
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+compaudit | xargs chmod g-w,o-w
+brew doctor
+
+brew install --cask iterm2
+
+exit
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+exit
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+curl -s "https://get.sdkman.io" | bash
+sdk install java 15.0.1.hs-adpt
+sdk install maven
+sdk install gradle
+sdk install scala
+sdk install sbt
+sdk install kotlin
+
+brew install --cask spectacle intellij-idea-ce visual-studio-code postman docker pycharm-ce dash
+
+brew install autojump tree p7zip
+
+brew install --cask transmission iina spotify whatsapp
+brew install --cask google-chrome
+brew install --cask quicklook-json quicklook-csv
+brew install --cask android-platform-tools
+brew install --cask serviio
+brew install terminal-notifier
