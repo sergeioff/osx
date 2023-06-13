ae6a7a8d0dc24919c155cc9b60e8772fdab06bf6
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2023-06-13 09:44:59 +0200

    feat(*): update steps

--- step1.sh
+++ step1.sh
@@ -1,41 +1,20 @@
 #!/bin/bash
 
-. ~/.sdkman/bin/sdkman-init.sh
-sdk install java 17.0.0-oracle
-sdk install maven
-sdk install gradle
-sdk install scala
-sdk install sbt
-sdk install kotlin
+FONTS_PATH=~/Library/Fonts
 
+brew analytics off
+brew doctor
+
+brew install --cask iterm2
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+curl -s "https://get.sdkman.io" | bash
 brew install nvm
-nvm install node v14.17.5
-
-brew install --cask rectangle intellij-idea-ce visual-studio-code postman docker pycharm-ce dash
-
-brew install wget autojump tree p7zip terminal-notifier youtube-dl
-
-brew install --cask transmission soda-player spotify 
-brew install --cask firefox # google-chrome google-chrome-canary
-
-brew install --cask android-platform-tools
-# brew install --cask virtualbox
-
-# brew install --cask serviio
-# brew install --cask protonvpn
-
-brew install --cask adobe-creative-cloud
-
-brew install --cask quicklook-json quicklook-csv
-brew install qlcolorcode qlstephen qlmarkdown quicklook-json suspicious-package
-xattr -d -r com.apple.quarantine ~/Library/QuickLook
-
-compaudit | xargs chmod g-w,o-w
-
-echo "Enter git user.name:"
-read NAME
-echo "Enter git user.email:"
-read EMAIL
-
-git config --global user.name $NAME
-git config --global user.email $EMAIL
---
+++ step2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sdk install java 20.0.1-oracle
+sdk install maven
+sdk install gradle
+sdk install scala
+sdk install sbt
+sdk install kotlin
+nvm install --lts node
+brew install --cask miniconda
+
+brew install wget autojump tree p7zip terminal-notifier
+brew install --cask rectangle intellij-idea-ce visual-studio-code postman pycharm-ce drawio dbeaver-community
+brew install podman # docker -> podman
+    
+brew install --cask zoom
+brew install --cask lulu utm qbittorrent calibre # ngrok virtualbox
+
+brew install --cask firefox google-chrome # google-chrome-canary
+
+brew install youtube-dl ffmpeg
+brew install --cask dash
+brew install --cask iina spotify # ace-link
+
+brew install --cask android-platform-tools
