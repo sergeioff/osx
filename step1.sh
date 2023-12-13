f8894eb165142e2581a633f231fb94af3760800f
Sergey <pogorelov.s.y@gmail.com>
2023-12-13 11:47:48 +0100

    fix(font-download): allow curl redirects

--- step1.sh
+++ step1.sh
@@ -9,10 +9,10 @@ brew install --cask iterm2
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-curl -O --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+curl -O -L --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -O -L --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -O -L --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -O -L --output-dir $FONTS_PATH https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
